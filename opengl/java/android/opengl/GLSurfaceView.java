85343b7cdd8bd1bd6cf4a6ca9d612208d2898f01
Shilin Victor <chrono.monochrome@gmail.com>
2021-03-21 05:49:43 +0300

    opengl/GLSurfaceView: debug chooseConfig
    Change-Id: Ic91c1561324c2abdf0a7fb6de07d198ce49d02ef

--- opengl/java/android/opengl/GLSurfaceView.java
+++ opengl/java/android/opengl/GLSurfaceView.java
@@ -955,26 +955,31 @@ public class GLSurfaceView extends SurfaceView implements SurfaceHolder.Callback
         @Override
         public EGLConfig chooseConfig(EGL10 egl, EGLDisplay display,
                 EGLConfig[] configs) {
+                Log.e(TAG, "chooseConfig init");
             for (EGLConfig config : configs) {
                 int d = findConfigAttrib(egl, display, config,
                         EGL10.EGL_DEPTH_SIZE, 0);
                 int s = findConfigAttrib(egl, display, config,
                         EGL10.EGL_STENCIL_SIZE, 0);
+                int r = findConfigAttrib(egl, display, config,
+                        EGL10.EGL_RED_SIZE, 0);
+                int g = findConfigAttrib(egl, display, config,
+                        EGL10.EGL_GREEN_SIZE, 0);
+                int b = findConfigAttrib(egl, display, config,
+                        EGL10.EGL_BLUE_SIZE, 0);
+                int a = findConfigAttrib(egl, display, config,
+                        EGL10.EGL_ALPHA_SIZE, 0);
                 if ((d >= mDepthSize) && (s >= mStencilSize)) {
-                    int r = findConfigAttrib(egl, display, config,
-                            EGL10.EGL_RED_SIZE, 0);
-                    int g = findConfigAttrib(egl, display, config,
-                             EGL10.EGL_GREEN_SIZE, 0);
-                    int b = findConfigAttrib(egl, display, config,
-                              EGL10.EGL_BLUE_SIZE, 0);
-                    int a = findConfigAttrib(egl, display, config,
-                            EGL10.EGL_ALPHA_SIZE, 0);
                     if ((r == mRedSize) && (g == mGreenSize)
                             && (b == mBlueSize) && (a == mAlphaSize)) {
                         return config;
                     }
                 }
+
+                Log.e(TAG, "chooseConfig:" + String.format("d=%d, s=%d, r=%d, g=%d, b=%d, a=%d", d, s, r, g, b, a));
+                Log.e(TAG, "chooseConfig:" + String.format("mD=%d, mS=%d, mR=%d, mG=%d, mB=%d, mA=%d",  mDepthSize, mStencilSize, mRedSize, mGreenSize, mBlueSize, mAlphaSize));
             }
+                Log.e(TAG, "chooseConfig exit");
             return null;
         }
 
